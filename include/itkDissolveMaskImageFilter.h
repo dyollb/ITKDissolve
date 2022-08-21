e285a27967ec27c7f49fea0b57f754fccbe8162a
Bryn Lloyd <lloyd@itis.swiss>
2022-08-21 10:51:29 +0200

    add UC as mask type

--- include/itkDissolveMaskImageFilter.h
+++ include/itkDissolveMaskImageFilter.h
@@ -29,9 +29,9 @@ namespace itk
  * \note The pixels inside the mask are dissolved by assigning values
  * from neighboring region. The filter processes in an ordered fashion
  * from the mask boundary towards the inside.
- * 
+ *
  * \author Bryn Lloyd, IT'IS Foundation, Switzerland
- * 
+ *
  * \ingroup Dissolve
  */
 template <typename TInputImage, typename TMaskImage>
@@ -40,6 +40,7 @@ class ITK_TEMPLATE_EXPORT DissolveMaskImageFilter : public ImageToImageFilter<TI
 public:
   /** Extract dimension from input and output image. */
   itkStaticConstMacro(InputImageDimension, unsigned int, TInputImage::ImageDimension);
+  itkStaticConstMacro(MaskImageDimension, unsigned int, TMaskImage::ImageDimension);
 
   static const unsigned int ImageDimension = InputImageDimension;
 
@@ -97,6 +98,7 @@ public:
   // Begin concept checking
   itkConceptMacro(InputEqualityComparableCheck, (Concept::EqualityComparable<InputPixelType>));
   itkConceptMacro(IntConvertibleToInputCheck, (Concept::Convertible<int, MaskPixelType>));
+  itkConceptMacro(SameDimensionCheck, (Concept::SameDimension<InputImageDimension, MaskImageDimension>));
   itkConceptMacro(InputConvertibleToOutputCheck, (Concept::Convertible<InputPixelType, OutputPixelType>));
   itkConceptMacro(InputOStreamWritableCheck, (Concept::OStreamWritable<InputPixelType>));
   // End concept checking
