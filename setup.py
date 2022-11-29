bc51cce68488efb57969d842361edb870b1884bb
Bryn Lloyd <lloyd@itis.swiss>
2022-11-29 09:20:13 +0100

    revert to rc4.post3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name="itk-dissolve",
-    version="1.0.6",
+    version="1.0.6.rc1",
     author="Bryn Lloyd",
     author_email="lloyd@itis.swiss",
     packages=["itk"],
@@ -44,5 +44,5 @@ setup(
     license="MIT",
     keywords="ITK InsightToolkit",
     url=r"https://github.com/dyollb/ITKDissolve",
-    install_requires=[r"itk-core>=5.3.0"],
+    install_requires=[r"itk>=5.3rc04.post3"],
 )
